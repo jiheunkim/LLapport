877863d639271d92e8900cb483d23c96e99b42f2
jiheunkim <ziheun54@gmail.com>
2023-09-29 09:47:40 +0900

    웹캠 연결

--- llapport/src/components/pages/Main.js
+++ llapport/src/components/pages/Main.js
@@ -1,39 +1,24 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../App.css';
+import Webcam from 'react-webcam';
 
 function Main() {
-  const videoRef = useRef(null);
-
-  // 사용자 웹캠에 접근
-  const getUserCamera = () => {
-    navigator.mediaDevices
-      .getUserMedia({ video: true })
-      .then((stream) => {
-        let video = videoRef.current;
-        video.srcObject = stream;
-        video.load(); // 비디오 다시 로드
-        video.play().catch((error) => {
-          console.log('비디오를 재생할 수 없습니다: ', error);
-        });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-  
-
-  useEffect(() => {
-    getUserCamera();
-  }, []);
-
   return (
     <>
       <div className="App-background">
         <div className='container'>
-            <br></br>
-            <h1 className='title'>Main</h1>
-            <video className='container' ref={videoRef}></video>
+          <br></br>
+          <h1 className='title'>Main</h1>
+          <Webcam
+            className='container'
+            audio={true} // 오디오 사용 여부
+            videoConstraints={{
+              width: 1280, // 원하는 비디오 너비
+              height: 720, // 원하는 비디오 높이
+              facingMode: 'user', // 전면 카메라 사용 (user) 또는 후면 카메라 사용 (environment)
+            }}
+          />
         </div>
       </div>
     </>
