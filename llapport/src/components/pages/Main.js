9535acdde2b21448ecf5b2539662dfdefa92a4dd
katekim512 <katekim512@konkuk.ac.kr>
2023-10-02 14:17:56 +0900

    수정

--- llapport/src/components/pages/Main.js
+++ llapport/src/components/pages/Main.js
@@ -8,6 +8,7 @@ function Main() {
   const webcamRef = useRef(null);
   const [recordedVideo, setRecordedVideo] = useState(null);
   const [recording, setRecording] = useState(false);
+  const [responseType, setResponseType] = useState(null);
 
   // 녹화 시작 함수
   const startRecording = () => {
@@ -34,12 +35,36 @@ function Main() {
     // 여기서 recordedVideo를 Django 서버로 업로드하는 로직 구현
   };
 
+  let circleColor = 'gray';
+  let message = '상태를 확인 중입니다...';
+
+  if (responseType === 1) {
+    circleColor = 'red';
+    message = '뇌졸증이 의심됩니다.';
+  } else if (responseType === 2) {
+    circleColor = 'yellow';
+    message = '정보가 정확하지 않으니 조금 있다가 다시 시도해주세요.';
+  } else if (responseType === 3) {
+    circleColor = 'green';
+    message = '안전한 상태입니다.';
+  }
+
   return (
     <>
       <div className="App-background">
-        <div className='container'>
+      <div className='container' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
           <br></br>
-          <h1 className='title'>Fast.LLapport 서비스 사용하기</h1>
+          {/* <h1 className='title'>Fast.LLapport 서비스 사용하기</h1> */}
+          <div style={{ marginTop: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <div style={{
+              width: '40px',
+              height: '40px',
+              borderRadius: '50%',
+              background: circleColor,
+              marginBottom:'20px',
+              marginTop:'-20px'
+            }}></div>
+          </div>
           <Webcam
             className='container'
             audio={true}
@@ -53,8 +78,11 @@ function Main() {
           {recording ? (
             <button className='btn' disabled>녹화 중...</button>
           ) : (
-            <button className='btn' onClick={startRecording}>녹화 시작</button>
+            <button className='btn' style={{ marginTop: '20px' }} onClick={startRecording}>녹화 시작</button>
           )}
+          <div style={{ marginTop: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <p>{message}</p>
+          </div>
           {recordedVideo && (
             <div>
               <video
