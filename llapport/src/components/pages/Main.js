5a82cc7b900b9627b773570ff2fb1f596fb33b22
katekim512 <katekim512@konkuk.ac.kr>
2023-10-04 19:52:04 +0900

    메인페이지 동그라미 크기 수정

--- llapport/src/components/pages/Main.js
+++ llapport/src/components/pages/Main.js
@@ -3,12 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../App.css';
 import Webcam from 'react-webcam';
 import RecordRTC from 'recordrtc';
+import '../Button.css';
 
 function Main() {
   const webcamRef = useRef(null);
   const [recordedVideo, setRecordedVideo] = useState(null);
   const [recording, setRecording] = useState(false);
-  const [responseType, setResponseType] = useState(null);
+  const [responseState, setResponseState] = useState(null);
+  const [responseDescription, setResponseDescription] = useState('');
 
   // 녹화 시작 함수
   const startRecording = () => {
@@ -33,20 +35,48 @@ function Main() {
   // 영상을 서버로 업로드하는 함수
   const uploadVideoToServer = () => {
     // 여기서 recordedVideo를 Django 서버로 업로드하는 로직 구현
+    // recordedVideo가 null이면 업로드하지 않음
+  if (!recordedVideo) {
+    return;
+  }
+
+  // FormData 객체 생성
+  const formData = new FormData();
+  formData.append('video', recordedVideo, 'recorded-video.webm'); // 'video'는 서버에서 파일을 받을 때의 키 이름, 'recorded-video.webm'은 업로드할 파일의 이름
+
+  // 서버 업로드 URL 설정 (서버의 업로드 API 엔드포인트 주소를 입력해야 합니다)
+  const uploadUrl = 'http://34.64.47.129:8000/llapport/showapi'; // 실제 서버의 업로드 API 주소로 변경해야 함
+
+  // 파일 업로드 요청 보내기
+  fetch(uploadUrl, {
+    method: 'POST',
+    body: formData,
+  })
+    .then(response => response.json())
+    .then(data => {
+      const { state, description } = data.data; // 서버 응답에서 상태와 설명 메시지 추출
+      setResponseState(state);
+      setResponseDescription(description);
+      // 서버 응답에 따른 로직을 추가할 수 있습니다.
+    })
+    .catch(error => {
+      console.error('업로드 실패:', error);
+      // 업로드 실패 시 처리할 로직을 추가할 수 있습니다.
+    });
   };
 
   let circleColor = 'gray';
   let message = '상태를 확인 중입니다...';
 
-  if (responseType === 1) {
+  if (responseState === 1) {
     circleColor = 'red';
-    message = '뇌졸증이 의심됩니다.';
-  } else if (responseType === 2) {
+    message = responseDescription; // 백엔드에서 온 설명 메시지를 사용
+  } else if (responseState === 2) {
     circleColor = 'yellow';
-    message = '정보가 정확하지 않으니 조금 있다가 다시 시도해주세요.';
-  } else if (responseType === 3) {
+    message = responseDescription;
+  } else if (responseState === 3) {
     circleColor = 'green';
-    message = '안전한 상태입니다.';
+    message = responseDescription;
   }
 
   return (
@@ -57,8 +87,8 @@ function Main() {
           {/* <h1 className='title'>Fast.LLapport 서비스 사용하기</h1> */}
           <div style={{ marginTop: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
             <div style={{
-              width: '40px',
-              height: '40px',
+              width: '25px',
+              height: '25px',
               borderRadius: '50%',
               background: circleColor,
               marginBottom:'20px',
@@ -78,7 +108,7 @@ function Main() {
           {recording ? (
             <button className='btn' disabled>녹화 중...</button>
           ) : (
-            <button className='btn' style={{ marginTop: '20px' }} onClick={startRecording}>녹화 시작</button>
+            <button className='btn' style={{ marginTop: '20px' }} onClick={startRecording}>RECORD</button>
           )}
           <div style={{ marginTop: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
             <p>{message}</p>
@@ -92,7 +122,9 @@ function Main() {
                 width={640}
                 height={360}
               ></video>
-              <button className='btn' onClick={uploadVideoToServer}>서버로 업로드</button>
+              <div style={{ marginTop: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <button className='btn' onClick={uploadVideoToServer}>START</button>
+              </div>
             </div>
           )}
         </div>
