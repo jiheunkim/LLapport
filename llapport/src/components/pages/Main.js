f5973fbe0ab27c88eadb601b3ed8a8d0378302d7
katekim512 <katekim512@konkuk.ac.kr>
2023-10-05 15:39:04 +0900

    main 수정

--- llapport/src/components/pages/Main.js
+++ llapport/src/components/pages/Main.js
@@ -11,12 +11,13 @@ function Main() {
   const [recording, setRecording] = useState(false);
   const [responseState, setResponseState] = useState(null);
   const [responseDescription, setResponseDescription] = useState('');
-
+  
   // 녹화 시작 함수
   const startRecording = () => {
     setRecording(true);
+    circleColor = 'gray';
     const options = {
-      mimeType: 'video/webm', // 미디어 타입 설정
+      mimeType: 'video/mp4', // 미디어 타입 설정
       bitsPerSecond: 128000, // 비트 속도 설정
     };
 
@@ -36,13 +37,14 @@ function Main() {
   const uploadVideoToServer = () => {
     // 여기서 recordedVideo를 Django 서버로 업로드하는 로직 구현
     // recordedVideo가 null이면 업로드하지 않음
-  if (!recordedVideo) {
+  if (!recordedVideo) {    
     return;
   }
+  circleColor='gray';
 
   // FormData 객체 생성
   const formData = new FormData();
-  formData.append('video', recordedVideo, 'recorded-video.webm'); // 'video'는 서버에서 파일을 받을 때의 키 이름, 'recorded-video.webm'은 업로드할 파일의 이름
+  formData.append('video', recordedVideo, 'recorded-video.mp4'); // 'video'는 서버에서 파일을 받을 때의 키 이름, 'recorded-video.webm'은 업로드할 파일의 이름
 
   // 서버 업로드 URL 설정 (서버의 업로드 API 엔드포인트 주소를 입력해야 합니다)
   const uploadUrl = 'http://34.64.47.129:8000/llapport/showapi'; // 실제 서버의 업로드 API 주소로 변경해야 함
@@ -54,9 +56,22 @@ function Main() {
   })
     .then(response => response.json())
     .then(data => {
-      const { state, description } = data.data; // 서버 응답에서 상태와 설명 메시지 추출
+      const { state, description_kor } = data; // 서버 응답에서 상태와 설명 메시지 추출
       setResponseState(state);
-      setResponseDescription(description);
+      // setResponseDescription(description);
+      // description이 배열인 경우 줄바꿈으로 나열
+      if (Array.isArray(description_kor)) {
+        let formattedDescription = '';
+        for (let i = 0; i < description_kor.length; i++) {
+          formattedDescription += description_kor[i];
+          if (i < description_kor.length - 1) {
+            formattedDescription += '\n';
+          }
+        }
+        setResponseDescription(formattedDescription);
+      } else {
+        setResponseDescription(description_kor);
+      }
       // 서버 응답에 따른 로직을 추가할 수 있습니다.
     })
     .catch(error => {
@@ -76,7 +91,7 @@ function Main() {
     message = responseDescription;
   } else if (responseState === 3) {
     circleColor = 'green';
-    message = responseDescription;
+    message = '';
   }
 
   return (
@@ -91,15 +106,18 @@ function Main() {
               height: '25px',
               borderRadius: '50%',
               background: circleColor,
-              marginBottom:'20px',
+              marginBottom:'10px',
               marginTop:'-20px'
             }}></div>
+          </div>
+          <div style={{ marginTop: '10px',marginBottom: '10px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <p>{message}</p>
           </div>
           <Webcam
             className='container'
             audio={true}
-            height={540}
-            width={960}
+            height={480}
+            width={640}
             videoConstraints={{
               width: 1280,
               height: 720,
@@ -112,9 +130,7 @@ function Main() {
           ) : (
             <button className='btn' style={{ marginTop: '20px' }} onClick={startRecording}>RECORD</button>
           )}
-          <div style={{ marginTop: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <p>{message}</p>
-          </div>
+          
           {recordedVideo && (
             <div>
               <video
